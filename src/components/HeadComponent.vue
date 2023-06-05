35b65ae9ac4b6083f557aac61e05bf065614fbf2
MirkoGLostia <giggi.torre@mail.com>
2023-06-05 16:08:43 +0200

    base-start

---
+++ src/components/HeadComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'HeadComponent'
+}
+</script>
+
+<template>
+  <section class="container">
+    <h1>
+      Yu-Gi-Oh Api
+    </h1>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  width: 90%;
+  height: 50px;
+  margin: 0 auto;
+}
+
+h1 {
+  font-size: 40px;
+}
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'MainComp'
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    width: 100%;
+    min-height: calc(100vh - 50px);
+    background-color: rgb(212, 143, 56);
+}
+
+.container {
+    width: 90%;
+    margin: 0 auto;
+}
+</style>
